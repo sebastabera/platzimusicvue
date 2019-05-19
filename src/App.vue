e44b4dcfc276679a76035dc2dd5bc77eb581435d
sebastabera <sebas03700@hotmail.com>
2019-05-19 12:38:23 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<template lang="pug">
+  #app
+    img(src='./assets/logo.png')
+    h1 PlatziMusic
+    select(v-model="selectedCountry")
+      option(v-for="country in countries" :value="country.value") {{country.name}}
+    spinner(v-show="loading")
+    ul
+      artist(v-for="artist in artists" v-bind:artist="artist" v-bind:key="artist.mbid")
+</template>
+
+<script>
+import Artist from "./components/Artist.vue";
+import getArtists from "./api";
+import Spinner from "./components/Spinner.vue";
+export default {
+  name: 'app',
+  data () {
+    return {
+      artists: [],
+      countries: [
+        {name: "Argentina", value:"argentina"},
+        {name: "Spain", value:"spain"},
+        {name: "Colombia", value:"colombia"}
+      ],
+      selectedCountry: "argentina",
+      loading: true
+    }
+  },
+  methods: {
+    refreshArtists() {
+      const self = this;
+      this.artists = [];
+      this.loading = true;
+      getArtists(this.selectedCountry)
+      .then(function(artists){
+        self.artists = artists;
+        self.loading = false;
+      });
+    }
+  },
+  mounted() {
+    this.refreshArtists()
+  },
+  components: {
+    Artist,
+    Spinner
+  },
+  watch: {
+    selectedCountry: function(){
+      this.refreshArtists()
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+  #app
+    font-family 'Avenir', Helvetica, Arial, sans-serif
+    -webkit-font-smoothing antialiased
+    -moz-osx-font-smoothing grayscale
+    text-align center
+    color red !important
+    margin-top 60px
+
+  h1, h2
+    font-weight normal
+
+  ul
+    list-style-type none
+    padding 0
+
+  li
+    display inline-block
+    margin 0 10px
+
+  a
+    color #42b983
+</style>
---
+++ src/api/config.js
@@ -0,0 +1,6 @@
+export default {
+    appname: 'platzimusic',
+    apiKey: '024e2f9b3bf534f0b5140f692da03517',
+    secret: '276c4938d629343ca55ea0de5655c7b7',
+    registeredTo: 'lstabera'
+}
